32fad3a444de1f514f4fdc0c0b544b71d2f0aa29
nizam <nixxam2@gmail.com>
2025-10-07 08:49:01 +0200

    Define módulo y dependencias para JavaFX, SQL y SLF4J
    - Declara requerimientos de módulos externos.
    - Exporta y abre paquetes para acceso FXML y modularidad.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,13 +1,12 @@
 module org.example {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.base;
-
-
+    requires java.sql;
+    requires org.slf4j;
 
     exports org.example;
     exports org.example.Modelo;
     exports org.example.Controlador;
-    opens org.example.Modelo to javafx.fxml, javafx.base;
-    opens org.example.Controlador to javafx.fxml, javafx.base;
+    opens org.example.Modelo to javafx.fxml;
+    opens org.example.Controlador to javafx.fxml;
 }
